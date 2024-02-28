c90c7f7418e5d261e47cb4a359f80dc20936de62
mahipal-haritwal <mahi98jat@gmail.com>
2024-02-29 02:28:45 +0530

    add search input

--- admin-ui/src/components/SearchComponent/SearchComponent.tsx
+++ admin-ui/src/components/SearchComponent/SearchComponent.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import "./SearchComponent.css";
 const SearchComponent = () => {
-  return <div>SearchComponent</div>;
+  return (
+    <input
+      className="search"
+      type="text"
+      onChange={(e) => console.log(e.target.value)}
+      placeholder="Search by name,email or role"
+    />
+  );
 };
 
 export default SearchComponent;
